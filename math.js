9ec531ad60771217f0e129c18d336b4c0bb51eeb
Max <139768623+zerotul777@users.noreply.github.com>
2023-07-20 19:41:14 +0300

    Add files via upload

---
+++ math.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
+function substract (a, b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function division (a, b) {
+    return a / b;
+}
